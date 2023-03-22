d798db7466a2e0fec4c3d214d119d62ca6cfcac7
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-03-22 16:15:20 +0800

    fixed event listeners

--- src/lib/components/PianoKey.svelte
+++ src/lib/components/PianoKey.svelte
@@ -4,9 +4,9 @@
 	export let keyClick: (note: string, type: string) => void;
 
 	const whiteKeyClasses =
-		'bg-primary-1 flex touch-none select-none justify-center items-end border-b-4 border-r border-primary-5 rounded-b h-[calc(100%+4px)] w-11 hover:bg-primary-4 active:bg-primary-5 transition-colors duration-300 ease-in-out';
+		'bg-primary-1 flex touch-none text-primary-8 select-none justify-center items-end border-b-4 border-r border-primary-5 rounded-b h-[calc(100%+4px)] w-12 hover:bg-primary-4 active:bg-primary-5 transition-colors duration-300 ease-in-out';
 	const blackKeyClasses =
-		'bg-primary-12 flex touch-none select-none justify-center items-end text-primary-1 text-xs  h-40 w-6 rounded-b -ml-3 -mr-3 z-10';
+		'bg-primary-12 flex touch-none select-none justify-center items-end text-primary-6 text-xs  h-40 w-7 rounded-b -ml-3.5 -mr-3.5 z-10';
 </script>
 
 <svelte:window
@@ -19,19 +19,21 @@
 		e.currentTarget.releasePointerCapture(e.pointerId);
 		keyClick(note, 'down');
 	}}
-	on:pointerup|preventDefault={() => {
+	on:pointerup|preventDefault={(e) => {
+		e.currentTarget.releasePointerCapture(e.pointerId);
 		keyClick(note, 'up');
 	}}
 	on:pointerenter|preventDefault={(e) => {
 		if (e.buttons === 1 || e.buttons === 3) {
 			keyClick(note, 'down');
 		}
-		keyClick(note, 'up');
 	}}
-	on:pointerleave|preventDefault={() => {
-		keyClick(note, 'up');
+	on:pointerleave|preventDefault={(e) => {
+		if (e.buttons === 1 || e.buttons === 3) {
+			keyClick(note, 'up');
+		}
 	}}
 	class={note.includes('#') ? blackKeyClasses : whiteKeyClasses}
 >
-	{note}</button
+	{note.includes('#') ? '' : note}</button
 >
