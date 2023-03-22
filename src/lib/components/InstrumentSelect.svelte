d28bb606e3c817f709a3472a9baa6994c0982593
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-03-22 16:16:09 +0800

    updated styles

--- src/lib/components/InstrumentSelect.svelte
+++ src/lib/components/InstrumentSelect.svelte
@@ -26,15 +26,15 @@
 	}
 </script>
 
-<div class="relative mt-1 w-52">
+<div class="relative mt-1 w-52 z-20">
 	<button
 		use:listbox.button
 		on:select={onSelect}
-		class="relative w-full cursor-default rounded-lg bg-primary-3 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+		class="relative w-full cursor-default rounded-full bg-primary-1 py-2 pl-4 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
 	>
 		<span class="block truncate">{$listbox.selected.name}</span>
 		<span
-			class="pointer-events-none material-symbols-rounded  absolute inset-y-0 right-0 flex items-center pr-2"
+			class="pointer-events-none material-symbols-rounded  absolute inset-y-0 right-0 flex items-center pr-3"
 		>
 			piano
 		</span>
@@ -44,9 +44,9 @@
 		<div transition:scale={{ duration: 250, easing: cubicInOut }}>
 			<ul
 				use:listbox.items
-				class="absolute z-20 mt-1 max-h-60 px-2 pr-0 w-full rounded-lg bg-surface-1 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+				class="absolute mt-1 h-64 px-2 pr-0 w-full rounded-lg bg-surface-1 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
 			>
-				<Svroller alwaysVisible={true} width="100%" margin={{ left: 20 }}>
+				<Svroller alwaysVisible={true} height="100%" width="100%" margin={{ left: 20 }}>
 					{#each instruments as value, i}
 						{@const active = $listbox.active === value}
 						{@const selected = $listbox.selected === value}
