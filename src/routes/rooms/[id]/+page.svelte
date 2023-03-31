0727841fd6fe4d9db0620a8a6c4526f7b6c3e055
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-01 00:34:00 +0800

    added id based rooms

---
+++ src/routes/rooms/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Piano from '$lib/components/Piano.svelte';
+	import { room } from '$lib/stores/webrtc/room';
+	import type { Action, NotesAction } from '$lib/stores/webrtc/room';
+
+	room.join({ roomId: $page.params.id, roomActions: ['notes'] });
+	const [sendNote, receiveNote] = room.actions.notes as Action<NotesAction>;
+</script>
+
+<Piano {sendNote} {receiveNote} />
