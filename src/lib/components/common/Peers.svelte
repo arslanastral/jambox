f7f1802e5595cca402207a4d80bdb612953a5fd5
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-07 17:03:04 +0800

    added peer sorting

--- src/lib/components/common/Peers.svelte
+++ src/lib/components/common/Peers.svelte
@@ -1,14 +1,21 @@
 <script>
 	import { peers } from '$lib/stores/webrtc/room';
-	import { pick } from 'nexusui';
+	import { selfId } from 'trystero';
+
+	$: sortedPeers = $peers.sort((a, b) => {
+		if (a.id === selfId || b.id === selfId) {
+			return -1;
+		}
+		return a.joined < b.joined ? -1 : 1;
+	});
 </script>
 
 <div class="flex items-center gap-4">
 	{#if $peers}
-		{#each $peers as item}
+		{#each sortedPeers as item, i}
 			<span
-				class="h-12 w-12 flex justify-center items-center ring-2 ring-primary-5 rounded-full bg-primary-4"
-				>{item.emoji}</span
+				class="h-12 w-12 text-xl flex justify-center items-center ring-2 ring-primary-5 rounded-full bg-primary-4"
+				class:ring-primary-9={item.id === selfId}>{item.emoji}</span
 			>
 		{/each}
 	{/if}
