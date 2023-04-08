440bd73adc711ff78066fb2e869079c816cec246
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-08 19:15:18 +0800

    added live indicator

--- src/lib/components/common/Logo.svelte
+++ src/lib/components/common/Logo.svelte
@@ -1,3 +1,19 @@
-<h1 class="text-3xl tracking-tight text-primary-12 font-logo">
-	JAMBOX <span id="innert" class="material-symbols-rounded text-primary-9"> music_note</span>
+<script>
+	import { peers } from '$lib/stores/webrtc/room';
+</script>
+
+<h1
+	class="text-3xl tracking-tight flex justify-between gap-2 items-center text-primary-12 font-logo"
+>
+	JAMBOX
+	{#if $peers.length}
+		<span
+			class="text-sm font-sans tracking-normal bg-red-400 text-white flex items-center justify-center px-[6px] py-[2px] rounded-md"
+		>
+			<span id="innert" class="material-symbols-rounded text-sm"> music_note </span>
+			LIVE
+		</span>
+	{:else}
+		<span id="innert" class="material-symbols-rounded text-primary-9"> music_note</span>
+	{/if}
 </h1>
