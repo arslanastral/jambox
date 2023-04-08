c2b2280fd4771c95495c30d72bc45833a8f9b8d9
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-08 17:03:08 +0800

    refactor key classes

--- src/lib/components/PianoKey.svelte
+++ src/lib/components/PianoKey.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { activeKeys } from '$lib/stores/piano';
 	import { currentInstrument } from '$lib/stores/tonejs/instruments';
+	import { peers } from '$lib/stores/webrtc/room';
 	import type { NotesAction } from '$lib/stores/webrtc/room';
+	import { selfId } from 'trystero';
 	export let note: string;
 	export let handleNote: (noteAction: NotesAction) => void;
 	export let keybind: string;
@@ -8,22 +11,28 @@
 	let active = false;
 
 	const whiteKeyClasses =
-		'bg-primary-1 flex touch-none text-primary-8 select-none justify-center items-end border-b-4 border-r border-primary-5 rounded-b h-[calc(100%+4px)] w-12 transition-all duration-150 ease-in-out';
+		'bg-primary-1 relative flex touch-none text-primary-8 select-none justify-center items-end border-b-4 border-r border-primary-5 rounded-b h-[calc(100%+4px)] w-12 transition-all duration-150 ease-in-out';
 	const blackKeyClasses =
-		'bg-primary-12 flex touch-none select-none justify-center items-end text-primary-6 text-xs  h-40 w-7 rounded-b -ml-3.5 -mr-3.5 z-10';
+		'bg-primary-12 relative flex touch-none select-none justify-center items-end text-primary-6 text-xs border-b-4 border-primary-12 h-40 w-7 rounded-b -ml-3.5 -mr-3.5 z-10 transition-all duration-150 ease-in-out';
+
+	const keyClasses = note.includes('#') ? blackKeyClasses : whiteKeyClasses;
+
+	$: pressedDown = { note, instrument: $currentInstrument, isPressed: true, player: selfId };
+	$: released = { note, instrument: $currentInstrument, isPressed: false, player: selfId };
+	$: activeClasses = active ? '!bg-amber-500 !border-amber-500 !border-b-0 !text-surface-1' : '';
 </script>
 
 <svelte:window
 	on:keydown={(e) => {
 		if (!e.repeat && e.key === keybind) {
 			active = true;
-			handleNote({ note, instrument: $currentInstrument, isPressed: true });
+			handleNote(pressedDown);
 		}
 	}}
 	on:keyup={(e) => {
 		if (!e.repeat && e.key === keybind) {
 			active = false;
-			handleNote({ note, instrument: $currentInstrument, isPressed: false });
+			handleNote(released);
 		}
 	}}
 />
@@ -32,29 +41,30 @@
 	on:pointerdown|preventDefault={(e) => {
 		e.currentTarget.releasePointerCapture(e.pointerId);
 		active = true;
-		handleNote({ note, instrument: $currentInstrument, isPressed: true });
+		handleNote(pressedDown);
 	}}
 	on:pointerup|preventDefault={(e) => {
 		active = false;
-		handleNote({ note, instrument: $currentInstrument, isPressed: false });
+		handleNote(released);
 	}}
 	on:pointerenter|preventDefault={(e) => {
 		if (e.buttons === 1) {
 			active = true;
-			handleNote({ note, instrument: $currentInstrument, isPressed: true });
+			handleNote(pressedDown);
 		}
 	}}
 	on:pointerleave|preventDefault={(e) => {
 		if (e.buttons === 1) {
 			active = false;
-			handleNote({ note, instrument: $currentInstrument, isPressed: false });
+			handleNote(released);
 		}
 	}}
-	class={note.includes('#') ? blackKeyClasses : whiteKeyClasses}
-	class:!bg-amber-500={active}
-	class:!border-amber-500={active}
-	class:!border-b-0={active}
-	class:!text-surface-1={active}
+	class="{keyClasses} {activeClasses}"
 >
-	{note.includes('#') ? '' : note}</button
->
+	{#if active && $peers.length}
+		<span
+			class="h-12 w-12 absolute -bottom-6 text-xl flex justify-center items-center ring-2 ring-primary-5 rounded-full bg-primary-4"
+			>😺</span
+		>
+	{/if}
+</button>
