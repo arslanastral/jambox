5bd47b69b74b9543ca59e5514094aba214e32dac
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-14 23:56:07 +0800

    added action button comp

---
+++ src/lib/components/common/ActionButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let icon = '';
+</script>
+
+<button
+	class="material-symbols-rounded w-10 h-10 text-primary-1 lg:text-primary-9 font-light flex items-center justify-center rounded-full"
+	on:click
+>
+	{icon}
+</button>
