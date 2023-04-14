f3c6310631526df8c77b087a18ca5fa4550bab3e
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-14 23:57:29 +0800

    updated logo size

--- src/lib/components/common/Logo.svelte
+++ src/lib/components/common/Logo.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <h1
-	class="text-3xl tracking-tight flex justify-between gap-2 items-center text-primary-12 font-logo"
+	class="text-xl tracking-tight flex gap-2 items-center text-primary-1 lg:text-primary-12 font-logo"
 >
 	JAMBOX
 	{#if $peers.length}
 		<span
-			class="text-sm font-sans tracking-normal bg-red-400 text-white flex items-center justify-center px-[6px] py-[2px] rounded-md"
+			class="text-xs font-sans tracking-normal bg-red-400 text-white flex items-center justify-center px-[5px] py-[2px] rounded-md"
 		>
-			<span id="innert" class="material-symbols-rounded text-sm"> music_note </span>
+			<span id="innert" class="material-symbols-rounded text-xs"> music_note </span>
 			LIVE
 		</span>
 	{:else}
-		<span id="innert" class="material-symbols-rounded text-primary-9"> music_note</span>
+		<span id="innert" class="material-symbols-rounded"> music_note</span>
 	{/if}
 </h1>
