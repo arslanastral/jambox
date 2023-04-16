ddaad56444f3204eb2accfeb1e22be0aaa84a149
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-17 00:21:56 +0800

    remove variable

--- src/lib/components/UX/DarkModeToggle.svelte
+++ src/lib/components/UX/DarkModeToggle.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import { theme } from '$lib/stores/UX/theme';
-
-	$: isDark = $theme.dark;
 </script>
 
 <button
 	class="material-symbols-rounded w-10 h-10 text-primary-1 lg:text-primary-9 dark:text-primary-9 font-light flex items-center justify-center rounded-full"
-	on:click={isDark ? theme.lightMode : theme.darkMode}
+	on:click={$theme.dark ? theme.lightMode : theme.darkMode}
 >
-	{#if isDark}
+	{#if $theme.dark}
 		light_mode
 	{:else}
 		dark_mode
