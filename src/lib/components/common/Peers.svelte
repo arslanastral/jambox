8419f651ef7b138b1933dc6537597d28b84aa770
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-16 15:27:42 +0800

    animate peers enter/leave

--- src/lib/components/common/Peers.svelte
+++ src/lib/components/common/Peers.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { tooltip } from '$lib/actions/tooltip';
 	import { peers, selfId } from '$lib/stores/webrtc/room';
-	import { scale } from 'svelte/transition';
+	import { crossfade, fade, fly, scale } from 'svelte/transition';
 	import Loader from './Loader.svelte';
+	import { flip } from 'svelte/animate';
+	import { quintOut } from 'svelte/easing';
 
 	$: sortedPeers = $peers.sort((a, b) => {
 		if (a.id === selfId || b.id === selfId) {
@@ -10,26 +12,46 @@
 		}
 		return a.joined < b.joined ? -1 : 1;
 	});
+
+	const [enter, leave] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
 </script>
 
 <div
 	class="flex items-center lg:justify-self-center col-span-3 xs:col-span-1 order-1 xs:order-none transition-all duration-150 ease-in-out min-w-min gap-3"
 >
 	<div class="flex flex-col xs:flex-row-reverse items-start xs:items-center gap-3">
-		{#if $peers.length === 1}
-			<div class="flex xs:flex-row-reverse items-center gap-3">
+		{#if sortedPeers.length === 1}
+			<div transition:scale={{ duration: 300 }} class="flex xs:flex-row-reverse items-center gap-3">
+				<Loader />
 				<span
 					class="text-base xs:text-sm lg:text-base font-light text-primary-1 dark:text-primary-9 lg:text-primary-9"
 					>Waiting For Friends</span
 				>
-				<Loader />
 			</div>
 		{/if}
 		{#if sortedPeers}
 			<div class="flex items-center justify-evenly gap-2">
-				{#each sortedPeers as item, i}
+				{#each sortedPeers as item, i (item.id)}
 					<span
-						transition:scale
+						in:enter={{ key: item.id }}
+						out:leave={{ key: item.id }}
+						animate:flip
 						use:tooltip={item.id === selfId ? 'You' : ''}
 						class="h-10 w-10 text-xl flex justify-center items-center rounded-full bg-primary-6 ring-primary-8"
 						class:ring-2={item.id === selfId}>{item.emoji}</span
