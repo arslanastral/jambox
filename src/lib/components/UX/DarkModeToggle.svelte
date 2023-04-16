d78492b6b239c7e83c6178180b7e637d4bf5b71c
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-16 15:26:04 +0800

    added dark mode toggle

---
+++ src/lib/components/UX/DarkModeToggle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { theme } from '$lib/stores/UX/theme';
+
+	$: isDark = $theme.dark;
+</script>
+
+<button
+	class="material-symbols-rounded w-10 h-10 text-primary-1 lg:text-primary-9 dark:text-primary-9 font-light flex items-center justify-center rounded-full"
+	on:click={isDark ? theme.lightMode : theme.darkMode}
+>
+	{#if isDark}
+		light_mode
+	{:else}
+		dark_mode
+	{/if}
+</button>
