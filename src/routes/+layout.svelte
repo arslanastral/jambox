56f9e9dc25671d274873d3eec06a006801988959
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-16 15:25:49 +0800

    set theme on mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { theme } from '$lib/stores/UX/theme';
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	onMount(() => {
+		theme.setTheme();
+	});
 </script>
 
 <slot />
