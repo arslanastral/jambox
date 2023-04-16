30025c74f9fd7a399c06139783fb8e4bc9c464a3
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-16 15:19:05 +0800

    use theme color in qr canvas

--- src/lib/actions/qrcode.ts
+++ src/lib/actions/qrcode.ts
@@ -1,12 +1,13 @@
 import QrCreator from 'qr-creator';
 
 export function QRCode(node: HTMLCanvasElement, text: string) {
+	const color = getComputedStyle(document.documentElement).getPropertyValue('--color-primary-11');
 	QrCreator.render(
 		{
 			text: text,
 			radius: 0.5, // 0.0 to 0.5
 			ecLevel: 'H', // L, M, Q, H
-			fill: 'blue', // foreground color
+			fill: color, // foreground color
 			background: null, // color or null for transparent
 			size: 250 // in pixels
 		},
