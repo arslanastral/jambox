d163d0591cb2f883098f49f773b60864f3511020
Arslan <81504779+arslanastral@users.noreply.github.com>
2023-04-18 14:20:59 +0800

    added web analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
 	import { theme } from '$lib/stores/UX/theme';
 	import { onMount } from 'svelte';
 	import '../app.css';
-
+	import { PUBLIC_APP_ENV } from '$env/static/public';
 	onMount(() => {
 		theme.setTheme();
 	});
 </script>
+
+<svelte:head>
+	{#if PUBLIC_APP_ENV === 'prod'}
+		<script
+			defer
+			src="https://static.cloudflareinsights.com/beacon.min.js"
+			data-cf-beacon={'{"token": "e271d0e3d28d4b35b50ca6e54345d7d5"}'}
+		></script>
+	{/if}
+</svelte:head>
 
 <slot />
